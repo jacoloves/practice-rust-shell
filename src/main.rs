18530e578aadf358ac833c5ca3f145c38ab1cbdb
jacoloves <5511068t@gmail.com>
2023-04-16 07:24:45 +0900

    shell built-ins

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::{
+    env,
     io::{stdin, stdout, Write},
+    path::Path,
     process::Command,
 };
 
@@ -15,8 +17,19 @@ fn main() {
         let command = parts.next().unwrap();
         let args = parts;
 
-        let mut child = Command::new(command).args(args).spawn().unwrap();
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    eprintln!("{}", e);
+                }
+            }
+            command => {
+                let mut child = Command::new(command).args(args).spawn().unwrap();
 
-        child.wait();
+                child.wait();
+            }
+        }
     }
 }
